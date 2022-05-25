54cf2f9cc79c04766748de37172753322fdfdf67
Summer Etherington <summersce23@gmail.com>
2022-05-25 14:41:55 -0600

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import ListDisplay from './ListDisplay'
+
+const Main = () => {
+
+    const [task, setTask] = useState('')
+    const [category, setCategory] = useState('')
+    const [list, setList] = useState([])
+
+    const handleChange = (e) => {
+        setTask(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setList([...list, {task: task, category: category}])
+        setTask('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type='text' placeholder='task' onChange={handleChange} value={task}></input>
+                <select onChange={(e) => setCategory(e.target.value)}>
+                    <option defaultValue disabled selected>category</option>
+                    <option value='chores'>Chores</option>
+                    <option value='errands' >Errands</option>
+                    <option value='work'>Work</option>
+                </select>
+                <button type='submit'>Add</button>
+            </form>
+            <ListDisplay list={list} setList={setList}/>
+        </div>
+    )
+}
+
+export default Main
